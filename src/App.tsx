1daf4a1849002b1108d61295ab82eb2502d9963d
Maksim <i@mfedyanin.ru>
2025-01-30 19:31:20 +0300

    stealth app

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
 import {Route, Routes} from "react-router-dom"
-import {Comments, SignUp, SignIn, Landing} from "./pages"
-import {ConfirmedEmail} from "./pages/ConfirmEmail"
-import {AudiencePage} from "./pages/Workspace/Audience"
-import {AboutCompanyPage} from "./pages/Workspace/AboutCompany"
-import {AboutYouPage} from "./pages/Workspace/AboutYou"
-import {ToneOfVoicePage} from "./pages/Workspace/ToneOfVoice"
-import {SettingsPage} from "./pages/Workspace/Settings"
-import {StripePage} from "./pages/StripePage"
 import './App.css'
+import {ML} from "./pages/ml.tsx"
 
 function App() {
   return <Routes>
-    <Route path="/" element={<AudiencePage />} />
-    <Route path="/signup" element={<SignUp />} />
-    <Route path="/signin" element={<SignIn />} />
-    <Route path="/workspace/audience" element={<AudiencePage />} />
-    <Route path="/workspace/about-company" element={<AboutCompanyPage />} />
-    <Route path="/workspace/about-you" element={<AboutYouPage />} />
-    <Route path="/workspace/tone-of-voice" element={<ToneOfVoicePage />} />
-    <Route path="/workspace/settings" element={<SettingsPage />} />
-    <Route path="/comments" element={<Comments />} />
-    <Route path="/landing" element={<Landing />} />
-    <Route path="/confirm-email/:token" element={<ConfirmedEmail />} />
-    <Route path="/payment" element={<StripePage />} />
+    <Route path="/" element={<ML />} />
   </Routes>
 }
 
---
+++ src/pages/ml.tsx
@@ -0,0 +1,55 @@
+import {useCallback, useRef, useState} from "react"
+import * as faceapi from '@vladmandic/face-api'
+import {Button} from "keep-react"
+
+export const ML = () => {
+  const [text, setText] = useState('')
+
+  const compareFaces = useCallback(async () => {
+    setText('load models')
+
+    await faceapi.nets.ssdMobilenetv1.loadFromUri('https://yastatic.net/s3/grocery-superapp/static/models/')
+    await faceapi.nets.faceLandmark68Net.loadFromUri('https://yastatic.net/s3/grocery-superapp/static/models/')
+    await faceapi.nets.faceRecognitionNet.loadFromUri('https://yastatic.net/s3/grocery-superapp/static/models/')
+
+    setText('load photos')
+    const img1 = await faceapi.bufferToImage((document.getElementById('photo') as HTMLInputElement).files[0])
+    const img2 = await faceapi.bufferToImage((document.getElementById('passport') as HTMLInputElement).files[0])
+
+    setText('try to detect')
+    const detections1 = await faceapi.detectSingleFace(img1).withFaceLandmarks().withFaceDescriptor()
+    const detections2 = await faceapi.detectSingleFace(img2).withFaceLandmarks().withFaceDescriptor()
+
+    if (!detections1 || !detections2) {
+      setText('Could not detect both faces')
+
+      console.error('Could not detect both faces')
+
+      return undefined
+    }
+
+    const distance = faceapi.euclideanDistance(detections1.descriptor, detections2.descriptor)
+
+    console.log('Distance:', distance)
+
+    setText(`Status: ${distance < 0.6 ? 'valid' : 'invalid'}`)
+
+    // Предположим, что если расстояние меньше 0.6, то это один и тот же человек
+    return distance < 0.6
+  }, [])
+
+  const checkFace = useCallback(async () => {
+    setText('Try to detect')
+
+    await compareFaces()
+  }, [])
+
+  return (
+    <div className="flex flex-col space-y-3">
+      <input id="passport" type="file" accept="image/*"/>
+      <input id="photo" type="file" accept="image/*"/>
+      <Button onClick={checkFace}>Check</Button>
+      <div>{text}</div>
+    </div>
+  )
+}
